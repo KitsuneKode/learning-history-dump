302d478b58a72b14a0966aa78269e3b5bb739b48
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2025-09-25 20:17:17 +0530

    add sign logic

--- Solana/jup-dapp/index.ts
+++ Solana/jup-dapp/index.ts
@@ -1,18 +1,29 @@
 import axios, { AxiosError } from "axios";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  sendAndConfirmTransaction,
+  Transaction,
+  VersionedTransaction,
+} from "@solana/web3.js";
+import bs58 from "bs58";
 
-const qty = 0.00001 * LAMPORTS_PER_SOL;
+const qty = 0.0000001 * LAMPORTS_PER_SOL;
 const JUP_URl = "https://lite-api.jup.ag";
 
 const SLIPPAGE = 5;
 
-const privateKey = "";
+const privateKey = bs58.decode(process.env.PRIVATE_KEY!);
 
 const inputMint = "So11111111111111111111111111111111111111112";
 const outPutTokenMintName = "TROLL";
 const outputMint = "5UUH9RTDiSpq6HKS6bp4NdU9PNJpXRXuiw6ShBTBhgH2";
 
-const publicKey = "FR4DL1qRhE6tu7nTzxEWPp3KJdLvm4aahbZBcoRyyX7K";
+const keyPair = Keypair.fromSecretKey(privateKey);
+
+const publicKey = keyPair.publicKey.toBase58();
+const connection = new Connection(process.env.RPC_ENDPOINT!, "confirmed");
 
 async function swap() {
   let config = {
@@ -64,15 +75,28 @@ async function requestUnsignedSwapTransaction(quoteData: string) {
     const marketResponse = await axios.request(config);
 
     if (marketResponse.data) {
-      console.log(marketResponse.data.swapTransaction);
+      console.log({ swapTransaction: marketResponse.data.swapTransaction });
+
+      const tx = Uint8Array.from(
+        Buffer.from(marketResponse.data.swapTransaction, "base64"),
+      );
+      await signSwapTransaction(tx);
     }
   } catch (error) {
     console.error((error as AxiosError).message);
   }
 }
 
-async function signSwapTransaction() {
-  let config = {};
+async function signSwapTransaction(transaction: Uint8Array) {
+  try {
+    const tx = VersionedTransaction.deserialize(transaction);
+    tx.sign([keyPair]);
+
+    const txId = await connection.sendTransaction(tx);
+    console.log(`https://explorer.solana.com/tx/${txId}?cluster=mainnet`);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 swap();
