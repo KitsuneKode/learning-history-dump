54513ba2f7e1f18d7167d48f68a218430543912e
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2025-09-25 19:24:32 +0530

    simple dapp init

---
+++ Solana/jup-dapp/index.ts
@@ -0,0 +1,75 @@
+import axios, { AxiosError } from "axios";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const qty = 0.00001 * LAMPORTS_PER_SOL;
+const JUP_URl = "https://lite-api.jup.ag";
+
+const SLIPPAGE = 5;
+
+const privateKey = "";
+
+const inputMint = "So11111111111111111111111111111111111111112";
+const outPutTokenMintName = "TROLL";
+const outputMint = "5UUH9RTDiSpq6HKS6bp4NdU9PNJpXRXuiw6ShBTBhgH2";
+
+const publicKey = "FR4DL1qRhE6tu7nTzxEWPp3KJdLvm4aahbZBcoRyyX7K";
+
+async function swap() {
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${JUP_URl}/swap/v1/quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${qty}&slippageBps=${SLIPPAGE}&userPublicKey=${publicKey}`,
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const quoteResponse = await axios.request(config);
+
+    if (quoteResponse.data) {
+      // console.log(quoteResponse.data);
+      console.log(
+        `You will get ${quoteResponse.data.outAmount} of ${outPutTokenMintName} for ${qty} of SOL `,
+      );
+
+      const quoteData = JSON.stringify(quoteResponse.data);
+      const ans = prompt("do you to swap? y/n ");
+
+      if (ans === "y") {
+        await requestUnsignedSwapTransaction(quoteData);
+      }
+    }
+  } catch (error) {
+    console.error((error as AxiosError).message);
+  }
+}
+
+async function requestUnsignedSwapTransaction(quoteData: string) {
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://lite-api.jup.ag/swap/v1/swap",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    data: quoteData,
+  };
+
+  try {
+    const marketResponse = await axios.request(config);
+
+    if (marketResponse.data) {
+      console.log(marketResponse.data);
+    }
+  } catch (error) {
+    console.error((error as AxiosError).message);
+  }
+}
+
+async function signSwapTransaction() {
+  let config = {};
+}
+
+swap();
